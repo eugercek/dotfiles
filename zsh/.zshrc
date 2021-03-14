1eec828482ba5bf6eb1ada798a8c6cd172a34eeb
EminUmutGercek <eumutgercek@gmail.com>
2021-03-15 01:51:10 +0300

    zsh changes

--- zsh/.zshrc
+++ zsh/.zshrc
@@ -15,7 +15,7 @@ export ZSH="/home/umut/.oh-my-zsh"
 # to know which specific one was loaded, run: echo $RANDOM_THEME
 # See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
 ZSH_THEME="powerlevel10k/powerlevel10k"
-#ZSH_THEME="spaceship"
+#ZSH_THEME="clean"
 
 # Set list of themes to pick from when loading at random
 # Setting this variable when ZSH_THEME=random will cause zsh to load
--- zsh_setup.sh
+++ zsh_setup.sh
@@ -1,8 +1,6 @@
-sudo dnf install zsh
+sudo dnf install zsh fzf zsh-autosuggestions zsh-syntax-highlighting vim -y
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-
-sudo dnf install fzf zsh-autosuggestions zsh-syntax-highlighting
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
