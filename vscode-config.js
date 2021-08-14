c16956063a4aebe811954b5581e2bcbf4d5601d3
Emin Umut Gerçek <eumutgercek@gmail.com>
2021-08-15 01:50:48 +0300

    vscode: Add current vscode config

---
+++ vscode-config.js
@@ -0,0 +1,14 @@
+{
+  "editor.cursorBlinking": "solid",
+  "keyboard.dispatch": "keyCode",
+  "files.autoSave": "afterDelay",
+  "files.autoSaveDelay": 100,
+  "workbench.sideBar.location": "right",
+  "terminal.integrated.fontFamily": "MesloLGS NF",
+  "vim.useSystemClipboard": true,
+  "omnisharp.organizeImportsOnFormat": true,
+  "csharp.referencesCodeLens.enabled": false,
+  "vim.useCtrlKeys": false,
+  "window.zoomLevel": 2,
+  "workbench.settings.editor": "json",
+}
