4107f181a5a136e6062d9e51c484887f33d0aa18
Emin Umut Gerçek <eumutgercek@gmail.com>
2021-09-23 00:10:01 +0300

    Organization and flameshot and vscode extensions

--- zsh_setup.sh
+++ zsh_setup.sh
@@ -2,11 +2,11 @@ sudo dnf install zsh
 
 chsh -s /bin/zsh
 
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 sudo dnf install fzf zsh-autosuggestions zsh-syntax-highlighting
 
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
