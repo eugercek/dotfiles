fc2cc172448d8882605ec0f8c5ab2ca05f7582c9
Emin Umut Gerçek <eumutgercek@gmail.com>
2021-12-24 14:53:36 +0300

    surfingkeys: Improve surfingkeys config

--- surfingkeys.js
+++ surfingkeys.js
@@ -1,4 +1,9 @@
-// ----- Variable configs -----
+settings.hintAlign = "left";
+//Hints.characters = 'yuiophjklnm'; // for right hand
+Hints.characters = 'fjdkrueisl'; // Home row without pinky
+
+cmap('<Ctrl-j>', '<Tab>');
+cmap('<Ctrl-k>', '<Shift-Tab>');
 
 settings.hintAlign = "left";
 //Hints.characters = 'yuiophjklnm'; // for right hand
@@ -17,11 +22,8 @@ let blockSites = [
 
 const leaderKey = ",";
 
-// ----- Util Functions -----
-
 const alt_key = (key) => leaderKey + key;
 
-// https://github.com/brookhong/Surfingkeys/blob/master/src/content_scripts/common/api.js#L132
 // `map` handles bad domain
 const key = (newKey, oldKey, domain) => {
   if (newKey) {
@@ -30,15 +32,14 @@ const key = (newKey, oldKey, domain) => {
   unmap(oldKey);
 };
 
-// ----- Handle collision of video specific keybindings and Surfingkeys -----
-
 let videoBlockedKeys = new RegExp(blockSites.join("|"), "i");
 let youtubeBlockedKeys = new RegExp(blockSites.join("|"), "i");
 
-videoKeys.forEach((i) => key(alt_key(i), i, videoBlockedKeys));
-youtubeKeys.forEach((i) => key(alt_key(i), i, youtubeBlockedKeys));
+videoKeys.forEach((i) => map(alt_key(i), i, videoBlockedKeys));
+youtubeKeys.forEach((i) => map(alt_key(i), i, youtubeBlockedKeys));
 
-// ------ Remap -----
+videoKeys.forEach((i) => unmap(i, videoBlockedKeys));
+youtubeKeys.forEach((i) => unmap(i, youtubeBlockedKeys));
 
 key("K", "R");
 key("H", "S");
@@ -46,34 +47,38 @@ key(";o", "<Ctrl-6>");
 key("L", "D");
 key("F", "gf");
 
-// ----- New Functionalities -----
+unmap('<Ctrl-j>');
+unmap('<Ctrl-h>');
+unmap('E');
+unmap('R');
+unmap('S');
+unmap('<Ctrl-6>');
+unmap('D');
+unmap('gf');
 
-mapkey("ye", "Copy src URL of an image", function () {
-  Hints.create("img[src]", function (element, event) {
-    Clipboard.write(element.src);
-  });
+mapkey('ye', 'Copy src URL of an image', function() {
+    Hints.create('img[src]',(element, event) =>  {
+        Clipboard.write(element.src);
+    });
 });
 
-mapkey("yme", "Copy multiple link URLs to the clipboard", function () {
-  var linksToYank = [];
-  Hints.create(
-    "img[src]",
-    function (element) {
-      linksToYank.push(element.src);
-      Clipboard.write(linksToYank.join("\n"));
-    },
-    { multipleHits: true }
-  );
+mapkey('yme', 'Copy multiple link URLs to the clipboard', function() {
+    let linksToYank = [];
+    Hints.create('img[src]', function(element) {
+        linksToYank.push(element.src);
+        Clipboard.write(linksToYank.join('\n'));
+    }, {multipleHits: true});
 });
 
-mapkey(";n", "Go to next episode", function next_episode() {
-  base_url = window.location.href;
-  ep_no = base_url.match(/(\d+)(?!.*\d)/)[0];
-  new_ep = parseInt(ep_no, 10) + 1;
-  n = base_url.lastIndexOf(ep_no);
-  new_url = base_url.slice(0, n) + base_url.slice(n).replace(ep_no, new_ep);
-  window.location = new_url;
-});
+mapkey(';n', 'Go to next episode',
+       function next_episode(){
+           base_url = window.location.href
+           ep_no = base_url.match(/(\d+)(?!.*\d)/)[0];
+           new_ep = parseInt(ep_no, 10) + 1;
+           n = base_url.lastIndexOf(ep_no);
+           new_url = base_url.slice(0, n) + base_url.slice(n).replace(ep_no, new_ep);
+           window.location = new_url;
+       });
 
 mapkey(";c", "Copy title and url for org mode", () => {
   let url = document.URL;
