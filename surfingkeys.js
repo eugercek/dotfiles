a4abfb4172f0ad3a4f42d571526a58acb8539cfc
Emin Umut Gerçek <eumutgercek@gmail.com>
2021-12-29 16:15:16 +0300

    surfingkeys: Bump and Fix
    https://github.com/brookhong/Surfingkeys/wiki/Migrate-your-settings-from-0.9.74-to-1.0

--- surfingkeys.js
+++ surfingkeys.js
@@ -1,13 +1,13 @@
 settings.hintAlign = "left";
 //Hints.characters = 'yuiophjklnm'; // for right hand
-Hints.characters = 'fjdkrueisl'; // Home row without pinky
+api.Hints.characters = 'fjdkrueisl'; // Home row without pinky
 
-cmap('<Ctrl-j>', '<Tab>');
-cmap('<Ctrl-k>', '<Shift-Tab>');
+api.cmap('<Ctrl-j>', '<Tab>');
+api.cmap('<Ctrl-k>', '<Shift-Tab>');
 
 settings.hintAlign = "left";
 //Hints.characters = 'yuiophjklnm'; // for right hand
-Hints.characters = "fjdkrueisl"; // Home row without pinky
+api.Hints.characters = "fjdkrueisl"; // Home row without pinky
 
 let videoKeys = ["v", "s", "d", "h", "l", "r"]; //Video Speed Controller Keys
 let youtubeKeys = ["i", "f", "c", "0"];
@@ -27,19 +27,19 @@ const alt_key = (key) => leaderKey + key;
 // `map` handles bad domain
 const key = (newKey, oldKey, domain) => {
   if (newKey) {
-    map(newKey, oldKey, domain);
+    api.map(newKey, oldKey, domain);
   }
-  unmap(oldKey);
+  api.unmap(oldKey);
 };
 
 let videoBlockedKeys = new RegExp(blockSites.join("|"), "i");
 let youtubeBlockedKeys = new RegExp(blockSites.join("|"), "i");
 
-videoKeys.forEach((i) => map(alt_key(i), i, videoBlockedKeys));
-youtubeKeys.forEach((i) => map(alt_key(i), i, youtubeBlockedKeys));
+videoKeys.forEach((i) => api.map(alt_key(i), i, videoBlockedKeys));
+youtubeKeys.forEach((i) => api.map(alt_key(i), i, youtubeBlockedKeys));
 
-videoKeys.forEach((i) => unmap(i, videoBlockedKeys));
-youtubeKeys.forEach((i) => unmap(i, youtubeBlockedKeys));
+videoKeys.forEach((i) => api.unmap(i, videoBlockedKeys));
+youtubeKeys.forEach((i) => api.unmap(i, youtubeBlockedKeys));
 
 key("J", "E");
 key("K", "R");
@@ -48,20 +48,20 @@ key(";o", "<Ctrl-6>");
 key("L", "D");
 key("F", "gf");
 
-unmap("<Ctrl-j>");
-unmap("<Ctrl-h>");
-unmap(";m")
+api.unmap("<Ctrl-j>");
+api.unmap("<Ctrl-h>");
+api.unmap(";m")
 
-unmap("C")
-unmap("<Ctrl-i>")
+api.unmap("C")
+api.unmap("<Ctrl-i>")
 
-mapkey('ye', 'Copy src URL of an image', function() {
+api.mapkey('ye', 'Copy src URL of an image', function() {
     Hints.create('img[src]',(element, event) =>  {
         Clipboard.write(element.src);
     });
 });
 
-mapkey('yme', 'Copy multiple link URLs to the clipboard', function() {
+api.mapkey('yme', 'Copy multiple link URLs to the clipboard', function() {
     let linksToYank = [];
     Hints.create('img[src]', function(element) {
         linksToYank.push(element.src);
@@ -69,7 +69,7 @@ mapkey('yme', 'Copy multiple link URLs to the clipboard', function() {
     }, {multipleHits: true});
 });
 
-mapkey(';n', 'Go to next episode',
+api.mapkey(';n', 'Go to next episode',
        function next_episode(){
            base_url = window.location.href
            ep_no = base_url.match(/(\d+)(?!.*\d)/)[0];
@@ -79,14 +79,12 @@ mapkey(';n', 'Go to next episode',
            window.location = new_url;
        });
 
-mapkey(";c", "Copy title and url for org mode", () => {
+api.mapkey(";c", "Copy title and url for org mode", () => {
   let url = document.URL;
   let title = document.title;
   Clipboard.write(`[[${url}][${title}]]`);
 });
 
-var mouse=[["=;fs=", "Display hints to focus scrollable elements"], ["=;di=", "Download image"], ["=af=", "Open a link in active new tab"], ["=O=", "Open detected links from text"], ["=q=", "Click on an Image or a button"]];
-var tabs=[["=T=", "Choose a tab"], ["=zr=", "zoom reset"], ["=zi=", "zoom in"], ["=zo=", "zoom out"], ["=<Alt-p>=", "Pin current tab"], ["=<Alt-m>=", "Mute current tab"], ["=on=", "Open new tab (=<Control-t>=)"]];
 const showCurrentTrainingBindings = () => {
     const messages = [...mouse, ...tabs].map(row => ( {
             binding: row[0].slice(1, -1),
@@ -97,4 +95,4 @@ const showCurrentTrainingBindings = () => {
     Front.showPopup(`<h1>${messages.join('<br>')} </h1>`);
 }
 
-mapkey(";?", "Show currently training keybindings", showCurrentTrainingBindings);
+api.mapkey(";?", "Show currently training keybindings", showCurrentTrainingBindings);
