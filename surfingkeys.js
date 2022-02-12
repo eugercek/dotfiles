43f5233bb5de21b6b9b8b8a8142fa7b09fd260e2
Emin Umut Gerçek <eumutgercek@gmail.com>
2022-02-12 23:42:18 +0300

    surfingkeys: Add theme switcher

--- surfingkeys.js
+++ surfingkeys.js
@@ -1,9 +1,9 @@
 settings.hintAlign = "left";
 //Hints.characters = 'yuiophjklnm'; // for right hand
-api.Hints.characters = 'fjdkrueisl'; // Home row without pinky
+api.Hints.characters = "fjdkrueisl"; // Home row without pinky
 
-api.cmap('<Ctrl-j>', '<Tab>');
-api.cmap('<Ctrl-k>', '<Shift-Tab>');
+api.cmap("<Ctrl-j>", "<Tab>");
+api.cmap("<Ctrl-k>", "<Shift-Tab>");
 
 settings.hintAlign = "left";
 //Hints.characters = 'yuiophjklnm'; // for right hand
@@ -50,39 +50,41 @@ key("F", "gf");
 
 api.unmap("<Ctrl-j>");
 api.unmap("<Ctrl-h>");
-api.unmap(";m")
-api.unmap(";m")
-
+api.unmap(";m");
+api.unmap(";m");
 
 //
-api.unmap("e")
+api.unmap("e");
 
-api.unmap("C")
-api.unmap("<Ctrl-i>")
+api.unmap("C");
+api.unmap("<Ctrl-i>");
 
-api.mapkey('ye', 'Copy src URL of an image', function() {
-    Hints.create('img[src]',(element, event) =>  {
-        api.Clipboard.write(element.src);
-    });
+api.mapkey("ye", "Copy src URL of an image", function () {
+  Hints.create("img[src]", (element, event) => {
+    api.Clipboard.write(element.src);
+  });
 });
 
-api.mapkey('yme', 'Copy multiple link URLs to the clipboard', function() {
-    let linksToYank = [];
-    Hints.create('img[src]', function(element) {
-        linksToYank.push(element.src);
-        api.Clipboard.write(linksToYank.join('\n'));
-    }, {multipleHits: true});
+api.mapkey("yme", "Copy multiple link URLs to the clipboard", function () {
+  let linksToYank = [];
+  Hints.create(
+    "img[src]",
+    function (element) {
+      linksToYank.push(element.src);
+      api.Clipboard.write(linksToYank.join("\n"));
+    },
+    { multipleHits: true }
+  );
 });
 
-api.mapkey(';n', 'Go to next episode',
-       function next_episode(){
-           base_url = window.location.href
-           ep_no = base_url.match(/(\d+)(?!.*\d)/)[0];
-           new_ep = parseInt(ep_no, 10) + 1;
-           n = base_url.lastIndexOf(ep_no);
-           new_url = base_url.slice(0, n) + base_url.slice(n).replace(ep_no, new_ep);
-           window.location = new_url;
-       });
+api.mapkey(";n", "Go to next episode", function next_episode() {
+  base_url = window.location.href;
+  ep_no = base_url.match(/(\d+)(?!.*\d)/)[0];
+  new_ep = parseInt(ep_no, 10) + 1;
+  n = base_url.lastIndexOf(ep_no);
+  new_url = base_url.slice(0, n) + base_url.slice(n).replace(ep_no, new_ep);
+  window.location = new_url;
+});
 
 api.mapkey(";c", "Copy title and url for org mode", () => {
   let url = document.URL;
@@ -91,16 +93,21 @@ api.mapkey(";c", "Copy title and url for org mode", () => {
 });
 
 const showCurrentTrainingBindings = () => {
-    const messages = [...mouse, ...tabs].map(row => ( {
-            binding: row[0].slice(1, -1),
-            description: row[1]
-        }))
-        .map(obj => `${obj.binding}\t\t\t\t${obj.description}`)
+  const messages = [...mouse, ...tabs]
+    .map((row) => ({
+      binding: row[0].slice(1, -1),
+      description: row[1],
+    }))
+    .map((obj) => `${obj.binding}\t\t\t\t${obj.description}`);
 
-    Front.showPopup(`<h1>${messages.join('<br>')} </h1>`);
-}
+  Front.showPopup(`<h1>${messages.join("<br>")} </h1>`);
+};
 
-api.mapkey(";?", "Show currently training keybindings", showCurrentTrainingBindings);
+api.mapkey(
+  ";?",
+  "Show currently training keybindings",
+  showCurrentTrainingBindings
+);
 
 api.mapkey(
   "ec",
@@ -112,3 +119,79 @@ api.mapkey(
   },
   { domain: /github.com/i }
 );
+
+solarizedDark = `
+.sk_theme {
+	background: #100a14dd;
+	color: #4f97d7;
+}
+.sk_theme tbody {
+	color: #292d;
+}
+.sk_theme input {
+	color: #d9dce0;
+}
+.sk_theme .url {
+	color: #2d9574;
+}
+.sk_theme .annotation {
+	color: #a31db1;
+}
+.sk_theme .omnibar_highlight {
+	color: #333;
+	background: #ffff00aa;
+}
+.sk_theme #sk_omnibarSearchResult ul li:nth-child(odd) {
+	background: #5d4d7a55;
+}
+.sk_theme #sk_omnibarSearchResult ul li.focused {
+	background: #5d4d7aaa;
+}
+.sk_theme #sk_omnibarSearchResult .omnibar_folder {
+	color: #a31db1;
+}
+`;
+
+zenbonse = `
+.sk_theme {
+  font-family: SauceCodePro Nerd Font, Consolas, Menlo, monospace;
+  font-size: 10pt;
+  background: #f0edec;
+  color: #2c363c;
+}
+.sk_theme tbody {
+  color: #f0edec;
+}
+.sk_theme input {
+  color: #2c363c;
+}
+.sk_theme .url {
+  color: #1d5573;
+}
+.sk_theme .annotation {
+  color: #2c363c;
+}
+.sk_theme .omnibar_highlight {
+  color: #88507d;
+}
+.sk_theme #sk_omnibarSearchResult ul li:nth-child(odd) {
+  background: #f0edec;
+}
+.sk_theme #sk_omnibarSearchResult ul li.focused {
+  background: #cbd9e3;
+}
+#sk_status,
+#sk_find {
+  font-size: 10pt;
+}
+`;
+
+settings.theme = `
+  @media (prefers-color-scheme: dark) {
+  ${solarizedDark}
+}
+  @media (prefers-color-scheme: light) {
+  ${zenbonse}
+}
+}
+`;
