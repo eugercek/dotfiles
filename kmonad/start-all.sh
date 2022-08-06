7ba19efce93e0541774833571c37557dc6a13c45
Emin Umut Gerçek <eumutgercek@gmail.com>
2022-08-06 14:54:18 +0300

    emacs,kmonad: Fix vterm add kmonad-all script

---
+++ kmonad/start-all.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+base=/home/umut/dotfiles/kmonad/kbd.kbd
+
+kbds=(
+	'/dev/input/by-path/platform-i8042-serio-0-event-kbd' # Builtin
+	'/dev/input/by-id/usb-OBINS_OBINS_AnnePro2_00000000000000000000000000000000-event-kbd' # AnnePro2 usb
+	"/dev/input/`grep -B 8 -A 4 12001 /proc/bus/input/devices | grep AnnePro -A 4 | grep -oE 'event[0-9]+'`" # Bluetooth
+)
+
+for i in "${kbds[@]}"
+do
+    fname="/tmp/kmonad-`basename $i`.kbd"
+    sed "s%KBD_FILE%\"${i}\"%" $base > $fname
+    if [ -e "$i" ];
+    then
+        echo "Started kmonad for $i"
+        /usr/bin/kmonad $fname &
+    else
+        echo "Not connected: $i"
+    fi
+
+done
+
+for job in `jobs -p`
+do
+    wait $job
+done
