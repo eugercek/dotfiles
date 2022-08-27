1bc0811c950b6949fee37772cdc219ba08df41df
Emin Umut Gerçek <eumutgercek@gmail.com>
2022-08-27 03:15:30 +0300

    Kmonad + Tmux
    - Add new keyboard
    - Add print screen key
    
    - Fix tmux color

--- kmonad/start-all.sh
+++ kmonad/start-all.sh
@@ -3,17 +3,16 @@
 base=/home/umut/dotfiles/kmonad/kbd.kbd
 
 kbds=(
-	"/dev/input/by-path/platform-i8042-serio-0-event-kbd" # Builtin
-	"/dev/input/by-id/usb-OBINLB_USB-HID_Keyboard_SN0000000001-event-kbd"
-	"/dev/input/`grep -B 8 -A 4 12001 /proc/bus/input/devices | grep AnnePro -A 4 | grep -oE 'event[0-9]+'`" # Bluetooth
+    "/dev/input/by-path/platform-i8042-serio-0-event-kbd"                                                     # Builtin
+    "/dev/input/by-id/usb-OBINLB_USB-HID_Keyboard_SN0000000001-event-kbd"                                     # AnnePro2 usb
+    "/dev/input/$(grep -B 8 -A 4 12001 /proc/bus/input/devices | grep AnnePro -A 4 | grep -oE 'event[0-9]+')" # Bluetooth
+    "/dev/input/by-id/usb-Logitech_USB_Receiver-if01-event-kbd"                                               # Logitech
 )
 
-for i in "${kbds[@]}"
-do
-    fname="/tmp/kmonad-`basename $i`.kbd"
-    sed "s%KBD_FILE%\"${i}\"%" $base > $fname
-    if [ -e "$i" ];
-    then
+for i in "${kbds[@]}"; do
+    fname="/tmp/kmonad-$(basename $i).kbd"
+    sed "s%KBD_FILE%\"${i}\"%" $base >$fname
+    if [ -e "$i" ]; then
         echo "Started kmonad for $i"
         /usr/bin/kmonad $fname &
     else
@@ -22,7 +21,6 @@ do
 
 done
 
-for job in `jobs -p`
-do
+for job in $(jobs -p); do
     wait $job
 done
