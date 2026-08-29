54a59a6a7137f0afaefdc34d5ca0c26abd733a39
Emin Umut Gecçek <eumutgercek@gmail.com>
2026-08-29 20:43:07 +0300

    ghostty: Cell Height

--- nvim/lua/plugins/render-markdown.lua
+++ nvim/lua/plugins/render-markdown.lua
@@ -4,11 +4,5 @@ vim.pack.add({
 
 require("render-markdown").setup({
 	render_modes = true,
-	-- paragraph = { left_margin = 0.5 },
-	code = {
-		sign = false,
-		width = "block",
-		left_pad = 1,
-		right_pad = 1,
-	},
+	code = { enabled = false },
 })
