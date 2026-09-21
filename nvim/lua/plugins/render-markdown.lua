99025f74826d0ac67caa3400580d8f2cc79d0042
Emin Umut Gercek <eumutgercek@gmail.com>
2026-09-21 20:41:52 +0300

    chore(nvim): Update packages
    - render-markdown.nvim now supports long table row wrapping, removed
      markdown-table-wrap.

--- nvim/lua/plugins/render-markdown.lua
+++ nvim/lua/plugins/render-markdown.lua
@@ -5,8 +5,6 @@ vim.pack.add({
 require("render-markdown").setup({
 	render_modes = true,
 	code = { enabled = false },
-	-- markdown-table-wrap.nvim draws tables now, it wraps long cells
-	pipe_table = { enabled = false },
 })
 
 nmap("<leader>tm", "<cmd>RenderMarkdown toggle<cr>", { desc = "Toggle render-markdown" })
