7f0f32da5e535e11f50e423640c5fc99f4599092
Emin Umut Gercek <eumutgercek@gmail.com>
2026-09-25 23:52:55 +0300

    nvim: Add <leader>ge for persisted git commit msg

--- nvim/lua/plugins/mini-git.lua
+++ nvim/lua/plugins/mini-git.lua
@@ -6,4 +6,34 @@ vim.pack.add({
 -- mini.git makes :Git commit open the message in this nvim (:wq commits, :q! aborts)
 require("mini.git").setup()
 
-nmap("<leader>gc", "<cmd>Git commit --verbose<cr>", { desc = "Commit" })
+-- Commit message draft, lives in .git/ so it's never committed
+local function draft_path()
+	local git_dir = vim.trim(vim.fn.system("git rev-parse --absolute-git-dir"))
+	return vim.v.shell_error == 0 and vim.fs.joinpath(git_dir, "COMMIT_DRAFT") or nil
+end
+
+nmap("<leader>ge", function()
+	local path = draft_path()
+	if not path then return vim.notify("Not a git repo", vim.log.levels.WARN) end
+	vim.cmd("botright 10split " .. vim.fn.fnameescape(path))
+	vim.bo.filetype = "gitcommit"
+end, { desc = "Edit commit draft" })
+
+nmap("<leader>gc", function()
+	local path = draft_path()
+	if path and vim.uv.fs_stat(path) then
+		vim.cmd("Git commit --verbose --edit -F " .. vim.fn.fnameescape(path))
+	else
+		vim.cmd("Git commit --verbose")
+	end
+end, { desc = "Commit" })
+
+-- Drop the draft once it's been committed
+vim.api.nvim_create_autocmd("User", {
+	pattern = "MiniGitCommandDone",
+	callback = function(ev)
+		if ev.data.git_subcommand ~= "commit" or ev.data.exit_code ~= 0 then return end
+		local path = draft_path()
+		if path then os.remove(path) end
+	end,
+})
