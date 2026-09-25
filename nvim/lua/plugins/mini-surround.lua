1be271a51ac4963849127ac8ea1a48dc11cae528
Emin Umut Gercek <eumutgercek@gmail.com>
2026-09-25 23:23:53 +0300

    nvim: Use `s` for mini.surround
    Writing `gsaiw"` was too long :D, trying just `s`

--- nvim/lua/plugins/mini-surround.lua
+++ nvim/lua/plugins/mini-surround.lua
@@ -2,14 +2,5 @@ vim.pack.add({
 	{ src = "https://github.com/echasnovski/mini.surround" },
 })
 
-require("mini.surround").setup({
-	mappings = {
-		add = "gsa",
-		delete = "gsd",
-		find = "gsf",
-		find_left = "gsF",
-		highlight = "gsh",
-		replace = "gsr",
-		update_n_lines = "gsn",
-	},
-})
+-- defaults: sa/sd/sr/sf/sF/sh/sn, and builtin s becomes <Nop> (cl does the same)
+require("mini.surround").setup()
